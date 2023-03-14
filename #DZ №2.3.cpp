7a152ede0c0218e0b470a2fc4eab3e5985dfdc20
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-03-14 07:36:58 +0400

    Create #DZ №2.3.cpp

---
+++ #DZ №2.3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+ using namespace std;  
+   
+ template <typename x>  
+ void kill_me_pls(int n) {  
+     x* a = new x[n];  
+     for (int i = 0; i < n; i++) {  
+         cin >> a[i];  
+     }  
+     for (int i = 0; i < n; i++) {  
+         cout << a[i] << " ";  
+     }  
+ }  
+ int main() {  
+     int n;  
+     cin >> n;  
+     kill_me_pls(n);  
+     return 0;  
+ } 
