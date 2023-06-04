89b6dccc222075de4bc68c434f4f2bd916ae9191
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-06-04 22:09:13 +0400

    Update 1.cpp

--- Dop.Dz/1.cpp
+++ Dop.Dz/1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
+void f(int n) {
+    if (n == 1) {
+        cout << "1\n";
+    } else {
+        for (int i = 1; i <= n; i++) {
+            cout << i;
+        }
+        cout << "\n";
+        f(n - 1);
+        for (int i =1; i <= n; i++) {
+            cout << i;
+        }
+        cout << "\n";
+    }
+}
 
+int main() {
+    f(5);
+    return 0;
+}
